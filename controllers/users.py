453cae9fc5c975857b211edfd1c3d1efb4e301ff
Laszlo Tarcsanyi <tarcsanyi.laszlo@gmail.com>
2013-10-20 22:12:45 +0200

    More visible highlight for user above todo limit

--- controllers/users.py
+++ controllers/users.py
@@ -7,5 +7,5 @@ def index():
         todo = db((db.to_review.tr_reviewer_id==user.id)&(db.to_review.tr_reviewed==None)).count()
         done = db((db.to_review.tr_reviewer_id==user.id)&(db.to_review.tr_reviewed!=None)).count()
         reported = db(db.to_review.tr_reporter_id==user.id).count()
-        result.append(TR(TD(user.first_name," ",user.last_name),TD(reported),TD(done),TD(todo),_class="warning" if todo > 3 else ""))
+        result.append(TR(TD(user.first_name," ",user.last_name),TD(reported),TD(done),TD(todo),_class="alert alert-error" if todo > 3 else ""))
     return dict(result=TABLE(result,_class="table"))
